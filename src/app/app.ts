9e7112e1ccce647f4878a8bf7cbcbe73b4454b91
johnapost <notimetobeidle@gmail.com>
2015-11-24 23:14:47 -0500

    2 way databinding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,22 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+
+class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ng-model)]='hero.name' placeholder='name' /></div>
+    </div>
+  `,
+  directives: [FORM_DIRECTIVES]
 })
 class AppComponent {
   public title = 'Tour of Heroes';
@@ -10,11 +24,6 @@ class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
-}
-
-class Hero {
-  id: number;
-  name: string;
 }
 
 bootstrap(AppComponent);
