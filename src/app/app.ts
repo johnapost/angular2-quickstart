b0395fbb9fe2a138ba4a9cf3f71097687a96f285
johnapost <notimetobeidle@gmail.com>
2015-11-24 23:23:30 -0500

    Display a list of heroes with ngFor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,35 @@
-import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor} from 'angular2/angular2';
 
 class Hero {
   id: number;
   name: string;
 }
 
+var HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+
+    <h2>My Heroes</h2>
+    <ul class='heroes'>
+      <li *ng-for='#hero of heroes'>
+        <span class='badge'>{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div>
@@ -16,7 +37,7 @@ class Hero {
       <div><input [(ng-model)]='hero.name' placeholder='name' /></div>
     </div>
   `,
-  directives: [FORM_DIRECTIVES]
+  directives: [FORM_DIRECTIVES, NgFor]
 })
 class AppComponent {
   public title = 'Tour of Heroes';
@@ -24,6 +45,7 @@ class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+  public heroes = HEROES;
 }
 
 bootstrap(AppComponent);
